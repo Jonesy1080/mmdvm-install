093ad1607e56949297849575606460f5fd1e907a
Jonesy1080 <36927389+Jonesy1080@users.noreply.github.com>
2023-09-20 15:04:17 -0400

    Update mmdvm-standalone-install.sh

--- mmdvm-standalone-install.sh
+++ mmdvm-standalone-install.sh
@@ -26,14 +26,15 @@ sudo systemctl start nginx
 #User: Add port forwarding through Fedora Server System Networking for port 80. Eventually maybe 443 if https gets enabled.
 
 #Fedora default: /usr/share/nginx/html
-#Pi-Star default: /
+#Pi-Star default: /var/www/dashboard
 cd /etc/nginx/sites-available
 #-OR- /etc/nginx/sites-enabled
-git clone https://repo.w0chp.net/WPSD-Dev/W0CHP-PiStar-Dash.git
+git clone https://repo.w0chp.net/WPSD-Dev/W0CHP-PiStar-Dash.git dashboard
 
 #nginx config files:
 #pi-star default: /etc/nginx/sites-enabled/pi-star (type: text)
-#Fedora default: /
+#Fedora default: /etc/nginx.nginx.conf
+sudo cp /etc/nginx/nginx.conf /etc/nginx-fedora-deafualt.conf
 #Fedora new: / / / /site-mmdvm
 
 cp
